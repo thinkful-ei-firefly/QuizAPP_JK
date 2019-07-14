9dd11f3ef7e6872f75a77d3611c448dd03f65cb4
Jess Doyle <jessica.e.doyle@gmail.com>
2019-07-13 20:16:29 -0400

    added listeners and js functionality to cycle through pages

--- quiz.js
+++ quiz.js
@@ -1,21 +1,20 @@
 'use strict';
 
-let qnum = 0; 
-//Start at zero so we can use the value of qnum as an index to pull the next question from the 'questions' array.
+let qnum = 0; //Tracks what question we're on. Starts at 0 because we'll use it as an index.
 
-let numCorrect = 0;
-//initialize at 0
+let numCorrect = 0; //Tracks how many questions the player has gotten right.
 
-let numIncorrect = 0;
-//initialize at 0
+let numIncorrect = 0; //Tracks how many questions the player has gotten wrong.
 
+
+//Data containing quiz questions
 const questions = [
   {question: 'What is the best way to defeat "IT"?',
     a1: 'Avoid',
     a2: 'Show no mercy',
     a3: 'Scream',
     a4: 'Don\'t believe',
-    correct: 'Avoid',
+    correct: 'Don\'t believe',
     funFact: 'The movie never addresses the real way IT was defeated. You\'ll have to read the book to find out.',
   },
   {question: 'What is "IT\'s" gender?',
@@ -55,14 +54,10 @@ const questions = [
 ];
 
 
-function startQuiz () {} 
-//listens for click on the 'Start Quiz' button
-//on pressing the 'Start Quiz' button, Calls askQuestion() to display the first question
 
-
+//empties the <main> element and appends an HTML block containing the current question to the <main>
+//updates the header to reflect current value of qnum
 function askQuestion () {
-  //$('main').html(`${questions[qnum].question}`);
-  //empties the <main> element and appends an HTML block containing the current question to the <main>
   $('main').html(`
     <div class="questionsAnswer">
       <h1>${questions[qnum].question}</h1>
@@ -88,7 +83,7 @@ function askQuestion () {
           </div>
 
           <div class="form-example">
-              <button type="submit" value="next">
+              <button type="submit" class="submit" value="next">
           </div>
 
       </form> 
@@ -97,66 +92,117 @@ function askQuestion () {
 
 
 
-  $('.questionNumber').text(`${qnum+1}`);  //updates the header to reflect current value of qnum
+  $('.questionNumber').text(`${qnum+1}`);  
 
-  //qnum++; //FOR TESTING PURPOSES ONLY. REMOVE THIS ONCE OTHER SECTIONS ARE FUNCTIONAL!
 }
 
 
 
-
+//listens for click on 'Submit' button
+//CURRENTLY just pulls a random number to see if the answer is true or false
+//NEEDS to actually check the answer
+//checks if the submitted answer matches the correct answer to the question
+//TODO: point Jquery selector at correct HTML item to extract form submission value
+//Calls feedback() to display the feedback page, passing it a boolean T/F based on whether the player got the question right
 function checkQuestion () {
-  if (questions[qnum].correct === $().val()) {
+  if (Math.random()<0.5) {
     
     feedback(true);
+
   } else {
     
     feedback(false);
+    
   }
 }
-//listens for click on 'Submit' button
-//checks if the submitted answer matches the correct answer to the question
-//TODO: point Jquery selector at correct HTML item to extract form submission value
-//Updates the values of numCorrect and numIncorrect
-//Calls feedback() to display the feedback page
 
 
+
+
+//takes a boolean as input
+//empties the <main> element and appends an HTML block displaying feedback from the question
+//feedback will vary depending on boolean input
+//updates the header to reflect current values of numCorrect and numIncorrect
 function feedback (bool) {
   if (bool) {
-    $('main').html(`Correct! Great job! Fun fact: ${questions[qnum].funFact}`);
+    $('main').html(`
+      <h2>Correct!</h2>
+      <p>Fun fact: ${questions[qnum].funFact}</p>
+      <button class="next">Next</button>
+    `);
+    
     numCorrect++;
     $('span.correct').text(`${numCorrect}`);
 
   } else {
-    $('main').html(`Sorry, the correct answer was: ${questions[qnum].correct}`);
+    $('main').html(`
+    <h2>Sorry!</h2>
+    <p>The correct answer was: ${questions[qnum].correct}</p>
+    <button class="next">Next</button>
+    `);
+    
     numIncorrect++;
     $('span.incorrect').text(`${numIncorrect}`);
 
   }
   
 }
-//takes a boolean as input
-//empties the <main> element and appends an HTML block displaying feedback from the question
-//feedback will vary depending on boolean input
-//updates the header to reflect current values of numCorrect and numIncorrect
 
 
-function feedbackNext () {}
-//listens for click on 'next' button
-//if qnum is not the last index of questions[], pressing the 'next' button should increment qnum by 1 and call askQuestion
-//if qnum is the last index of questions[], should call finalScore()
+//if qnum is not the last index of questions[], increments qnum by 1 and calls askQuestion
+//if qnum is the last index of questions[], calls finalScore()
+function feedbackNext () {
+  if (qnum < (questions.length - 1)) {
+    qnum++;
+    askQuestion();
+  } else {
+    finalScore();
+  }
+}
 
 
-function finalScore () {}
 //empties the <main> and appends an HTML block displaying the final score of the quiz
+function finalScore () {
+  $('main').html(`
+  <div>
+    <h2>Your final score was ${numCorrect} out of ${questions.length}</h2>
+    <button type="button" id="restartButton" class="restart">Try Again?</button>
+  </div>
+  `);
+}
 
 
-function restart () {}
-//listens for click on 'restart' button
 //empties the <main> and appends an HTML block with the starting page. Sets qnum, numCorrect, and numIncorrect to 0
 //updates header to reflect zeroed values of qnum, numCorrect, numIncorrect
+function restart () {
+  $('main').html(` 
+  <div> 
+    <h2>Can you make it out of Derry?</h2>
+    <button class="startQuiz" type="button">Begin</button>
+  </div>
+  `);
+
+  qnum = 0;
+  numCorrect = 0;
+  numIncorrect = 0;
+
+  $('.questionNumber').text(`${qnum+1}`);
+  $('span.correct').text(`${numCorrect}`);
+  $('span.incorrect').text(`${numIncorrect}`);
+
+}
 
 
-//all listeners should be tied to the <main> element
 
+//Here be the listeners. Quiet, please.
+$('main').on('click', '.startQuiz', function () {askQuestion();});
+
+$('main').on('submit', function () {
+  event.preventDefault();
+  checkQuestion();
+});
+
+$('main').on('click', '.next', function () {feedbackNext();});
+
+$('main').on('click', '.restart', function () {restart();});
 
